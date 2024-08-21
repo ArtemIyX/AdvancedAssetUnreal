a03577f46521f34e458aaa2fe38c37da3b856771
Wellsaik <artem.podorozhko@outlook.com>
2024-08-21 16:15:17 +0300

    Editor Plugin

---
+++ Source/AdvancedAssetEditor/Private/AdvancedAssetEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "AdvancedAssetEditor.h"
+
+#define LOCTEXT_NAMESPACE "FAdvancedAssetEditorModule"
+
+void FAdvancedAssetEditorModule::StartupModule()
+{
+    
+}
+
+void FAdvancedAssetEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FAdvancedAssetEditorModule, AdvancedAssetEditor)
